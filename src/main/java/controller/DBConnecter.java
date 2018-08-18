c6e7fd636f8c79fcf737847cb543b787730d6fa7
PornpatSantibuppakul <dottypurkt@gmail.com>
2018-08-18 22:48:59 +0700

    01 : Initialize repository on this project

---
+++ src/main/java/controller/DBConnecter.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.sql.*;
+
+public class DBConnecter {
+
+    private Connection connect;
+    private Statement statement;
+
+    DBConnecter() {
+        try {
+            connect = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/mysql","root","1234");
+            statement = connect.createStatement();
+            System.out.println("DATABASE CONNECTED!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet getResultSet(String query) throws SQLException{
+        return statement.executeQuery(query);
+    }
+}
---
+++ src/main/java/controller/test.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class test {
+    public static void main(String[] args) {
+        DBConnecter connecter = new DBConnecter();
+
+
+        try {
+            ResultSet resultSet = connecter.getResultSet("select * from user_list");
+
+            while (resultSet.next()) {
+                System.out.println(
+                        resultSet.getString("username"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
